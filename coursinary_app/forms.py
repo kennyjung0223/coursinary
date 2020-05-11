f841ccb67083f81a41bd73671130afc19b54b4b8
kennyjung0223 <kenrohs@gmail.com>
2020-05-10 22:55:39 -0700

    replaced text field with dropdown menu for code

--- coursinary_app/forms.py
+++ coursinary_app/forms.py
@@ -1,8 +1,34 @@
 from django import forms
 
-from .models import Course, Entry
+from .models import Subject, Course, Entry
+
+CODE_CHOICES = (
+	('i.e. ABC', 'i.e. ABC'),
+	('ANTH', 'ANTH'), ('BIO', 'BIO'),
+	('BIOE', 'BIOE'), ('CCST', 'CCST'),
+	('CHEM', 'CHEM'), ('CHN', 'CHN'),
+	('COGS', 'COGS'), ('CRES', 'CRES'),
+	('CRS', 'CRS'), ('CSE', 'CSE'),
+	('ECON', 'ECON'), ('EECS', 'EECS'),
+	('ENG', 'ENG'), ('ENGR', 'ENGR'),
+	('ENVE', 'ENVE'), ('ES', 'ES'),
+	('ESS', 'ESS'), ('FRE', 'FRE'),
+	('GASP', 'GASP'), ('HIST', 'HIST'),
+	('HS', 'HS'), ('IH', 'IH'),
+	('JPN', 'JPN'), ('MATH', 'MATH'),
+	('MBSE', 'MBSE'), ('ME', 'ME'),
+	('MGMT', 'MGMT'), ('MIST', 'MIST'),
+	('MSE', 'MSE'), ('NSED', 'NSED'),
+	('PH', 'PH'), ('PHIL', 'PHIL'),
+	('PHYS', 'PHYS'), ('POLI', 'POLI'),
+	('PSY', 'PSY'), ('QSB', 'QSB'),
+	('SOC', 'SOC'), ('SPAN', 'SPAN'),
+	('SPRK', 'SPRK'), ('WRI', 'WRI'),
+	)
 
 class CourseForm(forms.ModelForm):
+	#code = forms.ModelChoiceField(queryset=Subject.objects.values_list('code', flat=True))
+
 	class Meta:
 		model = Course
 		fields = [
@@ -17,7 +43,7 @@ class CourseForm(forms.ModelForm):
 				  }
 		widgets = {
 				  'text': forms.TextInput(attrs={'placeholder': 'i.e. ABC 123: Example Course'}),
-				  'code': forms.TextInput(attrs={'placeholder': 'i.e. ABC'}),
+				  'code': forms.Select(choices=CODE_CHOICES),
 				  'course_number': forms.TextInput(attrs={'placeholder': 'i.e. 123'}),
 				  }
 
